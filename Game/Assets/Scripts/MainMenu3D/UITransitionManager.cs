aa93687e5e028f27e43bbd788bf29a02f9e356c4
SergioGonzalezCastilla <sergiojgc2001@gmail.com>
2024-11-09 10:53:50 +0100

    Introducción de Botones y Movimiento de Cámaras - Menu3D
    Se han implementado botones con sus correspondientes cadenas de texto, se han implementado tres cámara con orden de prioridad apuntando a sus diferentes targets

---
+++ Game/Assets/Scripts/MainMenu3D/UITransitionManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class UITransitionManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera currentCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCamera.Priority++;
+    }
+
+    // Update is called once per frame
+    void UpdateCamera(CinemachineVirtualCamera target)
+    {
+        currentCamera.Priority--;
+        currentCamera = target;
+        currentCamera.Priority++;
+    }
+
+}
