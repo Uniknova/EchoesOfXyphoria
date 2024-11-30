b6ba71c91050152cc6e79443bb62345ea44124da
SergioGonzalezCastilla <sergiojgc2001@gmail.com>
2024-11-30 21:27:06 +0100

    Escena de Créditos
    Se ha creado e implementado la escena Créditos, la misma sirve para mostrar a los desarrolladores del juego y sus habilidades, también tiene añadida la URL que dirige a la web oficial

---
+++ Game/Assets/Resources/Créditos/EnlaceRedesSociales.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnlaceRedesSociales : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Openurl()
+    {
+        Application.OpenURL("bento.me/unknova?fbclid=PAZXh0bgNhZW0CMTEAAaZJZ5clkfXd8muEk-nL9-4CqqLoeZE3z0DYHpYG6jkMH-VwpXbj3lKS2T8_aem_5_Oa5MFgU1tvJZtrm9GsSQ");
+    }
+
+}
