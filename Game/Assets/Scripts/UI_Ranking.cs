1d514d0a2e131a8109400380b03f00b21efa2ca3
David Lozano <chavid54@gmail.com>
2024-12-11 01:38:11 +0100

    Ranking comienzo

--- Game/Assets/Scripts/UI_Ranking.cs
+++ Game/Assets/Scripts/UI_Ranking.cs
@@ -12,6 +12,10 @@ public class UI_Ranking : MonoBehaviour
     [SerializeField] private GameObject segundo;
     [SerializeField] private GameObject tercero;
 
+    private float posCasilla;
+    private float posCasillaX = 3.4f;
+    private float posTerryX = -577.99f;
+
     private static UI_Ranking instance;
 
     public static UI_Ranking Instance
@@ -29,14 +33,31 @@ public class UI_Ranking : MonoBehaviour
 
     public void ShowRank()
     {
+        posCasilla = 283.25f;
+
+        DataInfo d = DataInfo.Instance;
         List<Ranking> ranks = DataInfo.GetRanking();
+        Debug.Log(ranks.Count);
+
+        foreach (Ranking rank in ranks)
+        {
+            Debug.Log("si");
+            GameObject go = Instantiate(fondo);
+            go.transform.SetParent(transform);
+            go.transform.localPosition = new Vector3(posCasillaX, posCasilla, 0);
+            go = Instantiate(terry);
+            go.transform.SetParent(transform);
+            go.transform.localPosition = new Vector3(posTerryX, posCasilla, 0);
+            posCasilla -= 170f;
+        }
 
 
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("fdsafsa");
+        ShowRank();
     }
 
     // Update is called once per frame
